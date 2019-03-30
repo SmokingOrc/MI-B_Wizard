fc7de38bf20ac3e94b66da57b044becb309cbbee
SmokingOrc <kristof.627846@gmail.com>
2019-03-30 18:22:47 +0100

    design update, add & adept Main

--- app/src/main/java/com/example/mi_b_wizard/MainActivity.java
+++ app/src/main/java/com/example/mi_b_wizard/MainActivity.java
@@ -5,17 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    Button click = null;
-    Button buttonRegeln;
+    Button ButtonJoin = null;
+    Button ButtonRegeln;
+    EditText Userid;
+    String user;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        click = findViewById(R.id.btnClick);
-        click.setOnClickListener(new View.OnClickListener() {
+        ButtonJoin = findViewById(R.id.btnJoin); //Wechsel zu Join Game
+        ButtonJoin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, JoinGameActivity.class);
@@ -23,12 +28,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        buttonRegeln = findViewById(R.id.buttonRegeln);
+        ButtonRegeln = findViewById(R.id.btnRegeln);//Wechsel zu Regeln
+        ButtonRegeln.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i2 = new Intent(MainActivity.this, RulesActivity.class);
+                startActivity(i2);
+            }
+        });
+        Userid = findViewById(R.id.Userid);
+        user = Userid.getText().toString();// Username
     }
 
-    public void buttonRegeln(View view) {
+  /*  public void buttonRegeln(View view) {
         Intent i2 = new Intent(MainActivity.this, RulesActivity.class);
         startActivity(i2);
 
-    }
+    }*/
 }
