08d22bbbf322b47c5690ab10390f2f05596ec6d2
Erik <erikbr@edu.aau.at>
2019-04-02 12:39:14 +0200

    Added online users counter and connection alert.

--- app/src/main/java/com/example/mi_b_wizard/MainActivity.java
+++ app/src/main/java/com/example/mi_b_wizard/MainActivity.java
@@ -6,14 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.Socket;
 
 public class MainActivity extends AppCompatActivity {
     Button ButtonJoin = null;
     Button ButtonRegeln;
     Button ButtonCreate;
     EditText Userid;
+    TextView online;
     String user;
-
+    Socket mSocket;
+    String onLineUsers;
 
 
     @Override
@@ -21,6 +28,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButtonJoin = findViewById(R.id.btnJoin); //Wechsel zu Join Game
+        online = findViewById(R.id.online);
+
+        SocketIO app = (SocketIO) getApplication();
+        mSocket = app.getmSocket();
+
+        mSocket.on("connected" , onConnect);
+        mSocket.on("usersonline", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                onLineUsers = ("online users : " + Integer.toString((int) args[0]));
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println(onLineUsers);
+                        online.setText(onLineUsers);
+                    }
+                });
+            }
+        });
         ButtonJoin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-
+        mSocket.connect();
         ButtonRegeln = findViewById(R.id.btnRegeln);//Wechsel zu Regeln
         ButtonRegeln.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +76,20 @@ public class MainActivity extends AppCompatActivity {
         Userid = findViewById(R.id.Userid);
         user = Userid.getText().toString();// Username
     }
+
+    private Emitter.Listener onConnect = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("connected");
+                    Toast.makeText(MainActivity.this, "Connected to Wizard!",Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    };
+
 
   /*  public void buttonRegeln(View view) {
         Intent i2 = new Intent(MainActivity.this, RulesActivity.class);
