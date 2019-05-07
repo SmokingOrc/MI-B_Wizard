0d3054d4762d38b9439d26001b4100623f0853c1
Julia Hummel <juliahum@edu.aau.at>
2019-05-07 20:53:21 +0200

    added PredictedTrickActivity for Speechinput of the players

---
+++ app/src/main/java/com/example/mi_b_wizard/PredictedTricksActivity.java
@@ -0,0 +1,176 @@
+package com.example.mi_b_wizard;
+
+import android.content.Intent;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.mi_b_wizard.Data.Player;
+
+import java.util.ArrayList;
+
+
+public class PredictedTricksActivity extends AppCompatActivity implements RecognitionListener {
+
+    private TextView speechText;
+    private Button speechButton;
+    private ProgressBar progressBar;
+    private SpeechRecognizer speechRecognizer = null;
+    private Intent speechRecognizerIntent;
+    //static final int REQUEST_PERMISSION_KEY = 1;
+    private static final String LOG_TAG = "SpeechActivity";
+    Player player; //JoinGameActivity.owner;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_predicted_tricks);
+
+        speechText = findViewById(R.id.speechText);
+        progressBar = findViewById(R.id.progressBar);
+        speechButton = findViewById(R.id.speech_btn);
+        //TODO Methode to get real player name
+        player = new Player("Julia");
+
+        progressBar.setVisibility(View.INVISIBLE);
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        speechRecognizer.setRecognitionListener(this);
+
+
+        //SpeechButton to start and stop listening
+        speechButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startRecognitionActivity();
+                //Start listening
+                progressBar.setVisibility(View.VISIBLE);
+                speechRecognizer.startListening(speechRecognizerIntent);
+                //Stop listening
+                progressBar.setVisibility(View.INVISIBLE);
+                speechRecognizer.stopListening();
+            }
+        });
+    }
+
+    //Methods need to be override, because of the implementation of RecognitionListener(Abstract)
+    @Override
+    public void onBeginningOfSpeech() {
+        Log.d(LOG_TAG, "onBeginningOfSpeech");
+        progressBar.setVisibility(View.VISIBLE);
+    }
+    @Override
+    public void onBufferReceived(byte[] buffer) {
+        Log.d(LOG_TAG, "onBufferReceived: " + buffer);
+    }
+    @Override
+    public void onEndOfSpeech() {
+        Log.d(LOG_TAG, "onEndOfSpeech");
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+    @Override
+    public void onEvent(int event, Bundle params) {
+        Log.d(LOG_TAG, "onEvent" + event);
+    }
+    @Override
+    public void onReadyForSpeech(Bundle args) {
+        Log.d(LOG_TAG, "onReadyForSpeech");
+    }
+    @Override
+    public void onRmsChanged(float rmsdB) {
+        Log.d(LOG_TAG, "onRmsChanged");
+    }
+    @Override
+    public void onPartialResults(Bundle partialR) {
+        Log.d(LOG_TAG, "onPartialResults");
+        /*ArrayList<String> resultList = partialR.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        String result = resultList.get(0);*/
+    }
+    @Override
+    public void onResults(Bundle results) {
+        Log.d(LOG_TAG, "onResults");
+        ArrayList<String> resultList = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        String result = resultList.get(0);
+        speechText.setText(result);
+
+        player.updatePredictedTricks(result);
+        //sendPredictedTricks();
+    }
+
+    private void sendPredictedTricks() {
+        // TODO: send predicted tricks of player to hosting device
+        // if I am host of the game, then send to all
+        // else send to host
+    }
+
+    @Override
+    public void onError(int error) {
+        Log.d(LOG_TAG, "onError " + error);
+
+        String errorM = getErrorMessage(error);
+        progressBar.setVisibility(View.INVISIBLE);
+        speechText.setText(errorM);
+    }
+
+    public static String getErrorMessage(int error) {
+        String errorMessage;
+        switch (error) {
+            case SpeechRecognizer.ERROR_AUDIO:
+                errorMessage = "Audio error";
+                break;
+            case SpeechRecognizer.ERROR_NETWORK:
+                errorMessage = "Network error";
+                break;
+            case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                errorMessage = "Insufficient permissions";
+                break;
+            case SpeechRecognizer.ERROR_CLIENT:
+                errorMessage = "Client error";
+                break;
+            case SpeechRecognizer.ERROR_NO_MATCH:
+                errorMessage = "Didn't found a match";
+                break;
+            case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                errorMessage = "Network timeout";
+                break;
+            case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                errorMessage = "RecognitionService busy";
+                break;
+            case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                errorMessage = "Missing speech input";
+                break;
+            case SpeechRecognizer.ERROR_SERVER:
+                errorMessage = "Server error";
+                break;
+            default:
+                errorMessage = "Please try again.";
+                break;
+        }
+        return errorMessage;
+    }
+
+    private void startRecognitionActivity() {
+        //Intent to start the speechRecognition Activity
+        speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+        //Set the Callingpackage for identification of the application
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,
+                this.getPackageName());
+
+        //To inform the recognizer which speech model is prefered
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+        //To request partial results
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true);
+    }
+}
