5a71b65fe5dac8ae8922742836266ed5ce790b05
Christina Freundl <kim.possiblee007@gmail.com>
2019-06-05 22:33:45 +0200

    updated Layout in MainActivity

--- app/src/main/java/com/example/mi_b_wizard/MainActivity.java
+++ app/src/main/java/com/example/mi_b_wizard/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.mi_b_wizard.Data.Player;
